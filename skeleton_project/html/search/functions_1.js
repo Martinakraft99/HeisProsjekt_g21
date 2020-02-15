0fea3ab36fbb302272d4243cb7416c95877264ae
andregtu <andregtu@stud.ntnu.no>
2020-02-16 00:04:03 +0100

    Added doxygen

---
+++ skeleton_project/html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sigint_5fhandler',['sigint_handler',['../start__system_8h.html#a258e3b580e688a0cf46e4258525aeaf1',1,'start_system.c']]],
+  ['start_5fsystem',['start_system',['../start__system_8h.html#a6cb6cd3963cb32d63055dacfa689ed27',1,'start_system.c']]]
+];
