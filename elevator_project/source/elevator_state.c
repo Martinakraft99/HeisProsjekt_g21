570b400bdf87a2cef314d14b8163ed0694f00a28
Andreas Gudahl Tufte <andregtu@stud.ntnu.no>
2020-02-17 23:21:43 +0100

    Småfiks

--- elevator_project/source/elevator_state.c
+++ elevator_project/source/elevator_state.c
@@ -5,25 +5,3 @@
 #include "start_system.h"
 #include "hardware.h"
 
-// was static before
-void sigint_handler(int sig) {
-    (void)(sig);
-    printf("Terminating elevator\n");
-    hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-    exit(0);
-} 
-
-void start_system() {
-
-	int error = hardware_init();
-    if (error != 0) {
-        fprintf(stderr, "Unable to initialize hardware\n");
-        exit(1);
-    }
-    signal(SIGINT, sigint_handler);
-
-    printf("=== Running elevator program ===\n");
-    printf("Press the stop button on the elevator panel to exit\n");
-
-    hardware_command_movement(HARDWARE_MOVEMENT_DOWN);
-}
--- elevator_project/source/elevator_state.h
+++ elevator_project/source/elevator_state.h
@@ -1,18 +1,22 @@
 /**
  * @file
- * @brief Start up progress for elevator.
+ * @brief State machine.
  * 
  */
 
 /**
- * @brief Sigint handler.
+ * @brief If the elevator has no more steps to serve, it remains idle at its visited floor.
  */
-void sigint_handler(int sig);
+
+
 
 /**
  * @brief Start up of the system.
  */
-void start_system();
+
+/**
+ * @brief Start up of the system.
+ */
 
 
 
